3f965342895e83f5b1d394ec4c64d369c74c16df
Radu <radu.trandafir@logibec.com>
2020-10-27 16:41:14 -0400

    correct faker call

--- 13-javascript-development-environment-m13-exercise-files/materials/js-dev-env-demo/buildScripts/generateMockData.js
+++ 13-javascript-development-environment-m13-exercise-files/materials/js-dev-env-demo/buildScripts/generateMockData.js
@@ -7,10 +7,12 @@
 /* eslint-disable no-console */
 
 import jsf from 'json-schema-faker';
+import faker from 'faker';
 import {schema} from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk';
 
+jsf.extend('faker', () => faker);
 const json = JSON.stringify(jsf(schema));
 
 fs.writeFile("./src/api/db.json", json, function (err) {
